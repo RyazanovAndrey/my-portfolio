570bfc5c3604bdfec965836961998b7dc0d1d108
RyazanobAndrey <ryazanov.andrej@gmail.com>
2025-01-10 12:50:27 +0200

    12.50

---
+++ README.md
@@ -0,0 +1,2 @@
+# My PortFolio
+## My portfolio about skills and pet projects, what I did
---
+++ main.js
@@ -0,0 +1,67 @@
+const header = document.querySelector('.header')
+const swiperWrapper = document.querySelector('.swiper-wrapper')
+const themeBtn = document.querySelector('.theme-btn')
+
+if(localStorage.getItem('theme') == 'dark') {
+    document.body.classList.add('dark')
+}
+
+themeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark')
+
+    if(document.body.matches('.dark')) {
+        localStorage.setItem('theme', 'dark')
+    }else {
+        localStorage.setItem('theme', 'light')
+    }
+})
+
+const sliderInfo = [
+    { title: '{Design Art 1}', url: '', image: 'slider-demo.jpg' },
+    { title: '{Design Art 2}', url: '', image: 'slider-demo.jpg' },
+    { title: '{Design Art 3}', url: '', image: 'slider-demo.jpg' }
+]
+
+window.addEventListener('scroll', () => {
+    let currentScroll = window.scrollY
+
+    if (currentScroll > 0) {
+        header.classList.add('stop-header')
+    } else {
+        header.classList.remove('stop-header')
+    }
+
+})
+
+createSLider(sliderInfo)
+
+function createSLider(slider) {
+    slider.forEach(item => {
+        const newSlider = `
+        <div class="swiper-slide">
+            <div class="works-slider">
+                <div class="work-slider-wrapper">
+                    <img src="assets/images/${item.image}" alt="" class="works-slider-img">
+                    <a href="${item.url}" class="works-slider-demo">Live Demo</a>
+                </div>
+                <div class="works-slider-title">${item.title}</div>
+                <div class="works-slider-desc">Business card website Irina Boyko furniture designer</div>
+            </div>
+        </div>
+    `
+
+        swiperWrapper.insertAdjacentHTML('beforeend', newSlider)
+    })
+}
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 30,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+
+});
